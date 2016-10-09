4ffae97f62624b3cc06a896f5f09d7b22ab5b103
Joanna Grzywna <joanna.grzywna@gmail.com>
2016-10-09 20:18:09 +0200

    obsługa boardów Jira w profilu użytkownika

--- pmotoolsweb/app/model/userApp.js
+++ pmotoolsweb/app/model/userApp.js
@@ -1,13 +1,21 @@
 // app/models/board.js
 var mongoose = require('mongoose');
 
+var jiraBoardSchema = new mongoose.Schema(
+    {
+        boardid: Number,
+        name: String
+    }
+);
+
 var userAppSchema = new mongoose.Schema(
     {
         upn: {type : String , required : true},
         avatar: String,
         token: String,
         last_login: Date,
-        roles: [String]
+        roles: [String],
+        jiraBoards: [jiraBoardSchema]
     }
 );
 
