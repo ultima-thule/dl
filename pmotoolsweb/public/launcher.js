b0304c716c3691131868dcc2096b5be3082f57d3
ultima-thule <joanna.grzywna@gmail.com>
2017-03-28 19:56:39 +0200

    skrypt zdalny

---
+++ pmotoolsweb/public/launcher.js
@@ -0,0 +1,25 @@
+var pmoMenu = {
+
+    addScript: function(filename,callback){
+        var e=document.createElement('script');
+        e.type = 'text/javascript';
+        e.src = filename;
+        if(callback){
+            e.onloadDone=false;//for Opera
+            e.onload=function(){e.onloadDone=true;callback();};
+            e.onReadystatechange=function(){
+                if(e.readyState==='loaded'&& !e.onloadDone){
+                    e.onloadDone=true;callback();
+                }
+            }
+        }
+        if(typeof(e)!=='undefined'){
+            document.getElementsByTagName('head')[0].appendChild(e);
+        }
+    },
+
+    teams: ['#childrenspan66402618-0', '#childrenspan41629682-0']
+
+};
+
+pmoMenu.addScript('http://pmo.could.onet/PMOTool.js',function(){addButtons();});
